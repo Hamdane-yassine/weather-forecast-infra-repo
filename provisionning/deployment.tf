1bd92cc7b49e4cc9e5c689133d5e74c3cc9cce80
Hamdane10 <hamdaneyassine10@gmail.com>
2023-10-22 22:51:43 +0200

    initial infra

--- provisionning/deployment.tf
+++ provisionning/deployment.tf
@@ -1,1 +1,142 @@
-# Terraform's configuration for provisionning 3 WMs
+
+variable "gce_ssh_user" {
+  default = "hamdaneyassine10"
+}
+variable "gce_ssh_pub_key_file" {
+default = "~/.ssh/google_compute_engine.pub"
+}
+
+provider "google" {
+  credentials = file(var.deployKeyName)
+  project     = var.project
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_network" "default" {
+  name                    = "sdtd-network"
+  auto_create_subnetworks = "false"
+}
+
+resource "google_compute_subnetwork" "default" {
+  name            = "sdtd-sub-network"
+  network         = google_compute_network.default.name
+  ip_cidr_range   = "10.240.0.0/24"
+}
+
+resource "google_compute_firewall" "internal" {
+  name    = "allow-internal"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "tcp"
+  }
+
+  source_ranges = [ "10.240.0.0/24","10.200.0.0/16" ]
+}
+
+resource "google_compute_firewall" "external" {
+  name    = "allow-external"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "6443"]
+  }
+
+  source_ranges = [ "0.0.0.0/0" ]
+}
+
+resource "google_compute_address" "default" {
+  name = google_compute_network.default.name
+}
+
+resource "google_compute_instance" "master" {
+
+  count           = var.mastersVMSCount
+  name            = "master-${count.index}"
+  machine_type    = var.machineType
+  zone            = var.zone
+  can_ip_forward  = true
+
+  tags = ["master"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.name
+    network_ip = "10.240.0.1${count.index}"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  
+  service_account {
+    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+  }
+
+  metadata_startup_script = "apt-get install -y python"
+}
+
+resource "google_compute_instance" "worker" {
+
+  count        = var.workersVMSCount
+  name         = "worker-${count.index}"
+  machine_type = var.machineType
+  zone         = var.zone
+  can_ip_forward  = true
+
+  tags = ["worker"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.name
+    network_ip = "10.240.0.2${count.index}"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+  }
+
+  metadata = {
+    pod-cidr = "10.200.${count.index}.0/24"
+  }
+
+  metadata_startup_script = "apt-get install -y python"
+}
+
+resource "google_compute_project_metadata" "default" {
+  metadata = {
+    ssh-keys = "${var.gce_ssh_user}:${file(var.gce_ssh_pub_key_file)}"
+  }
+}
--- provisionning/variables.tf
+++ provisionning/variables.tf
@@ -1,1 +1,32 @@
-# useful variables for Terraform's configuration files
+variable "project" {
+  default = "stdt-project"
+}
+
+variable "region" {
+  default = "europe-west9"
+}
+
+variable "zone" {
+  default = "europe-west9-a"
+}
+
+variable "instanceName" {
+  default = "terraform-instance"
+}
+
+variable "deployKeyName" {
+  default = "../sdtd-sa.json"
+}
+
+variable "workersVMSCount" {
+  default = 2
+}
+
+variable "mastersVMSCount" {
+  default = 1
+}
+
+variable "machineType" {
+  default = "e2-standard-2"
+}
+
